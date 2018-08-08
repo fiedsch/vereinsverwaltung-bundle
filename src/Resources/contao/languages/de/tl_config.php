b3529bd2c5b6dd653865d8db610288ef7ec800fd
Andreas Fieger <fiedsch@ja-eh.at>
2018-08-08 08:53:30 +0200

    add labels

---
+++ src/Resources/contao/languages/de/tl_config.php
@@ -0,0 +1,8 @@
+<?php
+
+$GLOBALS['TL_LANG']['tl_liga']['vereinsverwaltung_legend'] = 'Vereinsverwaltung';
+
+$GLOBALS['TL_LANG']['tl_liga']['teampage'] = [
+    'Mannschaftsseite',
+    'Seite, auf der ein Modul vom Typ "Mannschaftsseitenreader" eingebunden ist.'
+];

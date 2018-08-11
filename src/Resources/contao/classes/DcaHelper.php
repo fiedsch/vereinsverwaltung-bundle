890296125e0a959564c91207810a26b51cb3bb01
Andreas Fieger <fiedsch@ja-eh.at>
2018-08-11 09:04:21 +0200

    remove unused return value

--- src/Resources/contao/classes/DcaHelper.php
+++ src/Resources/contao/classes/DcaHelper.php
@@ -12,7 +12,7 @@ class DcaHelper
     public static function generateMonthOfBirth($varValue, DataContainer $dc)
     {
         $month = Date::parse("m", $varValue);
-        $dbResult = Database::getInstance()
+        Database::getInstance()
             ->prepare("UPDATE tl_member SET monthOfBirth =? WHERE id=?")
             ->execute($month, $dc->id);
         return $varValue;
